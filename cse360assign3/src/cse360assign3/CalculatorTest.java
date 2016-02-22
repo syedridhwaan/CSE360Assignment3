8b282474b19da0dbbf47c495cfd3a8708e869e9e
Syed Ridhwaan <sridhwaa@asu.edu>
2016-02-22 07:02:19 -0700

    Here are the inital base files for CSE360 Assignment 3

---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,57 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cse360assign3.Calculator;
+
+public class CalculatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testCalculator() {
+		Calculator tester = new Calculator();
+		assertNotNull(tester);
+	}
+
+	@Test
+	public final void testGetTotal() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSubtract() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testMultiply() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDivide() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetHistory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
